82bed4bdf52b1f339dea7f6a9ff9e1c35a4c83f7
bansal-dhruv <dhruvbansal13999@gmail.com>
2019-07-10 17:01:53 +0530

    files added

---
+++ gesture_mouse.py
@@ -0,0 +1,196 @@
+from imutils import face_utils
+from helper import *
+import numpy as np
+import pyautogui as pag
+import imutils
+import dlib
+import cv2
+
+# Thresholds and consecutisve frame length for triggering the mouse action.
+MOUTH_AR_THRESH = 0.6
+MOUTH_AR_CONSECUTIVE_FRAMES = 15
+EYE_AR_THRESH = 0.19
+EYE_AR_CONSECUTIVE_FRAMES = 15
+WINK_AR_DIFF_THRESH = 0.04
+WINK_AR_CLOSE_THRESH = 0.19
+WINK_CONSECUTIVE_FRAMES = 10
+
+# Initialize the frame counters for each action as well as 
+# booleans used to indicate if action is performed or not
+MOUTH_COUNTER = 0
+EYE_COUNTER = 0
+WINK_COUNTER = 0
+INPUT_MODE = False
+EYE_CLICK = False
+LEFT_WINK = False
+RIGHT_WINK = False
+SCROLL_MODE = False
+ANCHOR_POINT = (0, 0)
+WHITE_COLOR = (255, 255, 255)
+YELLOW_COLOR = (0, 255, 255)
+RED_COLOR = (0, 0, 255)
+GREEN_COLOR = (0, 255, 0)
+BLUE_COLOR = (255, 0, 0)
+BLACK_COLOR = (0, 0, 0)
+
+# Initialize Dlib's face detector (HOG-based) and then create
+# the facial landmark predictor
+shape_predictor = "model/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+
+# Grab the indexes of the facial landmarks for the left and
+# right eye, nose and mouth respectively
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(nStart, nEnd) = face_utils.FACIAL_LANDMARKS_IDXS["nose"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+# Video capture
+vid = cv2.VideoCapture(0)
+resolution_w = 1366
+resolution_h = 768
+cam_w = 640
+cam_h = 480
+unit_w = resolution_w / cam_w
+unit_h = resolution_h / cam_h
+
+while True:
+    
+    _, frame = vid.read()
+    frame = cv2.flip(frame, 1)
+    frame = imutils.resize(frame, width=cam_w, height=cam_h)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    rects = detector(gray, 0)
+
+    # Loop over the face detections
+    if len(rects) > 0:
+        rect = rects[0]
+    else:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        continue
+
+    shape = predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+
+    # Find left and right eye coordinate
+    mouth = shape[mStart:mEnd]
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+    nose = shape[nStart:nEnd]
+
+    # left is right, right is left.
+    temp = leftEye
+    leftEye = rightEye
+    rightEye = temp
+    mar = mouth_aspect_ratio(mouth)
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+    ear = (leftEAR + rightEAR) / 2.0
+    diff_ear = np.abs(leftEAR - rightEAR)
+
+    nose_point = (nose[3, 0], nose[3, 1])
+
+    
+    mouthHull = cv2.convexHull(mouth)
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    cv2.drawContours(frame, [mouthHull], -1, (0,0,255), 1)
+    cv2.drawContours(frame, [leftEyeHull], -1, (0,0,255), 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, (0,0,255), 1)
+
+    for (x, y) in np.concatenate((mouth, leftEye, rightEye), axis=0):
+        cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
+        
+    # Check to see if the eye aspect ratio is below the blink
+    # threshold, and if so, increment the blink frame counter
+    if diff_ear > WINK_AR_DIFF_THRESH:
+
+        if leftEAR < rightEAR:
+            if leftEAR < EYE_AR_THRESH:
+                WINK_COUNTER += 1
+
+                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                    pag.click(button='left')
+
+                    WINK_COUNTER = 0
+
+        elif leftEAR > rightEAR:
+            if rightEAR < EYE_AR_THRESH:
+                WINK_COUNTER += 1
+
+                if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+                    pag.click(button='right')
+
+                    WINK_COUNTER = 0
+        else:
+            WINK_COUNTER = 0
+    else:
+        if ear <= EYE_AR_THRESH:
+            EYE_COUNTER += 1
+
+            if EYE_COUNTER > EYE_AR_CONSECUTIVE_FRAMES:
+                SCROLL_MODE = not SCROLL_MODE
+                EYE_COUNTER = 0
+
+                
+        else:
+            EYE_COUNTER = 0
+            WINK_COUNTER = 0
+
+    if mar > MOUTH_AR_THRESH:
+        MOUTH_COUNTER += 1
+
+        if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
+            # if the alarm is not on, turn it on
+            INPUT_MODE = not INPUT_MODE
+            MOUTH_COUNTER = 0
+            ANCHOR_POINT = nose_point
+
+    else:
+        MOUTH_COUNTER = 0
+
+    if INPUT_MODE:
+        cv2.putText(frame, "TAKING INPUT", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+        x, y = ANCHOR_POINT
+        nx, ny = nose_point
+        w, h = 60, 35
+        multiple = 1
+        cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
+        cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
+
+        dir = direction(nose_point, ANCHOR_POINT, w, h)
+        cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+        
+        drag = 18
+        if dir == 'right':
+            pag.moveRel(drag, 0)
+        elif dir == 'left':
+            pag.moveRel(-drag, 0)
+        elif dir == 'up':
+            if SCROLL_MODE:
+                pag.scroll(40)
+            else:
+                pag.moveRel(0, -drag)
+        elif dir == 'down':
+            if SCROLL_MODE:
+                pag.scroll(-40)
+            else:
+                pag.moveRel(0, drag)
+
+    if SCROLL_MODE:
+        cv2.putText(frame, 'SCROLLING', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # If the `Esc` key was pressed, break from the loop
+    if key == 27:
+        break
+
+# Do a bit of cleanup
+cv2.destroyAllWindows()
+vid.release()
