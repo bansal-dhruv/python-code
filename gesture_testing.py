869f3b07ed3e681e1d82baa93e177e649b1d7d40
manish2397 <rawatmanish2397@gmail.com>
2019-09-05 23:50:37 +0530

    mouse is smoother now

--- gesture_testing.py
+++ gesture_testing.py
@@ -15,6 +15,7 @@ WINK_AR_DIFF_THRESH = 0.04
 WINK_AR_CLOSE_THRESH = 0.19
 WINK_CONSECUTIVE_FRAMES = 10
 
+DRAG_LIST=[1,1,1,5,5,5,10,10,10,10,10,10,15,15,15,15,20]
 DEGREE_COUNTER = 0
 MOUTH_COUNTER = 0
 EYE_COUNTER = 0
@@ -50,6 +51,7 @@ cam_w = 640
 cam_h = 480
 unit_w = resolution_w / cam_w
 unit_h = resolution_h / cam_h
+continue_drag=0
 
 def eye_aspect_ratio(eye):
     # Compute the euclidean distances between the two sets of
@@ -100,191 +102,199 @@ def setAnchor(nx,ny):
     ANCHOR_POINT = ( nx, ny )
 
 
-while True:
+if __name__  == "__main__" :
+    global counter_drag
+    while True:
 
-    if SCROLL_MODE :
-        if FUTURE <= time.time():
-            SCROLL_MODE=False
-            print("Scroll Mode Deactivated")
+        if SCROLL_MODE :
+            if FUTURE <= time.time():
+                SCROLL_MODE=False
+                print("Scroll Mode Deactivated")
 
-    
-    _, frame = vid.read()
-    frame = cv2.flip(frame, 1)
-    frame = imutils.resize(frame, width=cam_w, height=cam_h)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the grayscale frame
-    rects = detector(gray, 0)
+        _, frame = vid.read()
+        frame = cv2.flip(frame, 1)
+        frame = imutils.resize(frame, width=cam_w, height=cam_h)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Loop over the face detections
-    if len(rects) > 0:
-        rect = rects[0]
-    else:
+        # Detect faces in the grayscale frame
+        rects = detector(gray, 0)
+
+        # Loop over the face detections
+        if len(rects) > 0:
+            rect = rects[0]
+        else:
+            cv2.imshow("Frame", frame)
+            key = cv2.waitKey(1) & 0xFF
+            continue
+
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        # Find left and right eye coordinate
+        mouth = shape[mStart:mEnd]
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        nose = shape[nStart:nEnd]
+        nose_line = shape[27:31]
+
+        # left is right, right is left.
+        temp = leftEye
+        leftEye = rightEye
+        rightEye = temp
+        mar = mouth_aspect_ratio(mouth)
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        diff_ear = np.abs(leftEAR - rightEAR)
+
+        nose_point = (nose[3, 0], nose[3, 1])
+
+
+        mouthHull = cv2.convexHull(mouth)
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [mouthHull], -1, (0,0,255), 1)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0,0,255), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0,0,255), 1)
+
+        for (x, y) in np.concatenate((mouth, leftEye, rightEye,nose_line), axis=0):
+            cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
+
+        radians = math.atan2(nose_line[0][1]-nose_line[3][1], nose_line[0][0]-nose_line[3][0] )
+        degrees = -1*math.degrees(radians)
+        # print(degrees)
+
+        # Right click or left click
+        # done !!!!!!!!!!
+        if (not INPUT_MODE ) or (not SCROLL_MODE) :
+            if degrees<=66:
+                if DEGREE_COUNTER>=5 :
+                    pag.click(button='right')
+                    print("Right Click!")
+                    DEGREE_COUNTER = 0
+                else :
+                    DEGREE_COUNTER+=1
+            elif degrees>=96:
+                if DEGREE_COUNTER>=5 :
+                    pag.click(button='left')
+                    print("Left Click!")
+                    DEGREE_COUNTER = 0
+                else :
+                    DEGREE_COUNTER+=1
+            else:
+                DEGREE_COUNTER=0
+
+           # if diff_ear > WINK_AR_DIFF_THRESH:
+        #     if leftEAR < rightEAR:
+        #         if leftEAR < EYE_AR_THRESH:
+        #             WINK_COUNTER += 1
+        #             if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+        #                 pag.click(button='left')
+        #                 WINK_COUNTER = 0
+        #     elif leftEAR > rightEAR:
+        #         if rightEAR < EYE_AR_THRESH:
+        #             WINK_COUNTER += 1
+        #             if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
+        #                 pag.click(button='right')
+        #                 WINK_COUNTER = 0
+        #     else:
+        #         WINK_COUNTER = 0
+        # else:
+
+
+        # Scrolling Mode !!!!
+        # done!!!
+        if not INPUT_MODE and not SCROLL_MODE:
+            if ear <= EYE_AR_THRESH:
+                EYE_COUNTER += 1
+                # print('Eyes_closed_once')
+                if EYE_COUNTER > EYE_AR_CONSECUTIVE_FRAMES:
+                    SCROLL_MODE = True#not SCROLL_MODE
+                    EYE_COUNTER = 0
+                    set_FUTURE()
+                    ANCHOR_POINT = nose_point
+                    print('Scrolling_mode_Activated!!!')
+            else:
+                EYE_COUNTER = 0
+            # WINK_COUNTER = 0
+
+
+        # mouth opening
+        # done!!!!!
+        if not SCROLL_MODE:
+            if mar > MOUTH_AR_THRESH:
+                MOUTH_COUNTER += 1
+
+                if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
+                    # if the alarm is not on, turn it on
+                    INPUT_MODE = not INPUT_MODE
+                    MOUTH_COUNTER = 0
+                    ANCHOR_POINT = nose_point
+            else:
+                MOUTH_COUNTER = 0
+
+        # movement for mouse
+        if INPUT_MODE:
+            cv2.putText(frame, "TAKING INPUT", (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED_COLOR, 2)
+            x, y =ANCHOR_POINT
+            nx, ny = nose_point
+            w, h = 60, 35
+            multiple = 1
+            cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
+            cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
+
+            dir = direction(nose_point, ANCHOR_POINT, w, h)
+            cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+
+            drag = 10
+            if(continue_drag>=16):
+                continue_drag=16
+
+            if dir == 'right':
+                pag.moveRel(DRAG_LIST[continue_drag], 0)
+            elif dir == 'left':
+                pag.moveRel(-DRAG_LIST[continue_drag], 0)
+            elif dir == 'up':
+                if SCROLL_MODE:
+                    pag.scroll(40)
+                else:
+                    pag.moveRel(0,-DRAG_LIST[continue_drag])
+            elif dir == 'down':
+                if SCROLL_MODE:
+                    pag.scroll(-40)
+                else:
+                    pag.moveRel(0,DRAG_LIST[continue_drag])
+            # setAnchor(nx, ny)
+            print(ANCHOR_POINT ," ", nose_point)
+            continue_drag+=1
+        else:
+            continue_drag=0
+        # for scrolling
+        if SCROLL_MODE:
+            cv2.putText(frame, 'SCROLLING', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+            x, y =ANCHOR_POINT
+            nx, ny = nose_point
+            w, h = 60, 35
+            multiple = 1
+            cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
+            cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
+
+            dir = direction(nose_point, ANCHOR_POINT, w, h)
+            cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
+            if dir == 'up':
+                pag.scroll(10)
+            elif dir == 'down':
+                pag.scroll(-10)
+
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
-        continue
 
-    shape = predictor(gray, rect)
-    shape = face_utils.shape_to_np(shape)
-
-    # Find left and right eye coordinate
-    mouth = shape[mStart:mEnd]
-    leftEye = shape[lStart:lEnd]
-    rightEye = shape[rStart:rEnd]
-    nose = shape[nStart:nEnd]
-    nose_line = shape[27:31]
-
-    # left is right, right is left.
-    temp = leftEye
-    leftEye = rightEye
-    rightEye = temp
-    mar = mouth_aspect_ratio(mouth)
-    leftEAR = eye_aspect_ratio(leftEye)
-    rightEAR = eye_aspect_ratio(rightEye)
-    ear = (leftEAR + rightEAR) / 2.0
-    diff_ear = np.abs(leftEAR - rightEAR)
-
-    nose_point = (nose[3, 0], nose[3, 1])
-
-    
-    mouthHull = cv2.convexHull(mouth)
-    leftEyeHull = cv2.convexHull(leftEye)
-    rightEyeHull = cv2.convexHull(rightEye)
-    cv2.drawContours(frame, [mouthHull], -1, (0,0,255), 1)
-    cv2.drawContours(frame, [leftEyeHull], -1, (0,0,255), 1)
-    cv2.drawContours(frame, [rightEyeHull], -1, (0,0,255), 1)
-
-    for (x, y) in np.concatenate((mouth, leftEye, rightEye,nose_line), axis=0):
-        cv2.circle(frame, (x, y), 2, GREEN_COLOR, -1)
- 
-    radians = math.atan2(nose_line[0][1]-nose_line[3][1], nose_line[0][0]-nose_line[3][0] )
-    degrees = -1*math.degrees(radians) 
-    # print(degrees)
-
-    # Right click or left click
-    # done !!!!!!!!!!
-    if (not INPUT_MODE ) or (not SCROLL_MODE) :
-        if degrees<=66:
-            if DEGREE_COUNTER>=5 :
-                pag.click(button='right')
-                print("Right Click!")
-                DEGREE_COUNTER = 0
-            else :
-                DEGREE_COUNTER+=1
-        elif degrees>=96:
-            if DEGREE_COUNTER>=5 :
-                pag.click(button='left')
-                print("Left Click!")
-                DEGREE_COUNTER = 0
-            else :
-                DEGREE_COUNTER+=1
-        else:
-            DEGREE_COUNTER=0
-
-       # if diff_ear > WINK_AR_DIFF_THRESH:       
-    #     if leftEAR < rightEAR:
-    #         if leftEAR < EYE_AR_THRESH:
-    #             WINK_COUNTER += 1
-    #             if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
-    #                 pag.click(button='left')
-    #                 WINK_COUNTER = 0
-    #     elif leftEAR > rightEAR:
-    #         if rightEAR < EYE_AR_THRESH:
-    #             WINK_COUNTER += 1
-    #             if WINK_COUNTER > WINK_CONSECUTIVE_FRAMES:
-    #                 pag.click(button='right')
-    #                 WINK_COUNTER = 0
-    #     else:
-    #         WINK_COUNTER = 0
-    # else:
-
-    
-    # Scrolling Mode !!!!
-    # done!!!
-    if not INPUT_MODE and not SCROLL_MODE:
-        if ear <= EYE_AR_THRESH:
-            EYE_COUNTER += 1
-            # print('Eyes_closed_once')
-            if EYE_COUNTER > EYE_AR_CONSECUTIVE_FRAMES:
-                SCROLL_MODE = True#not SCROLL_MODE
-                EYE_COUNTER = 0
-                set_FUTURE()                
-                ANCHOR_POINT = nose_point
-                print('Scrolling_mode_Activated!!!')                
-        else:
-            EYE_COUNTER = 0
-        # WINK_COUNTER = 0
+        # If the `Esc` key was pressed, break from the loop
+        if key == 27:
+            break
 
 
-    # mouth opening
-    # done!!!!!
-    if not SCROLL_MODE:
-        if mar > MOUTH_AR_THRESH:
-            MOUTH_COUNTER += 1
+    cv2.destroyAllWindows()
+    vid.release()
 
-            if MOUTH_COUNTER >= MOUTH_AR_CONSECUTIVE_FRAMES:
-                # if the alarm is not on, turn it on
-                INPUT_MODE = not INPUT_MODE
-                MOUTH_COUNTER = 0
-                ANCHOR_POINT = nose_point
-        else:
-            MOUTH_COUNTER = 0
-
-    # movement for mouse
-    if INPUT_MODE:
-        cv2.putText(frame, "TAKING INPUT", (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, RED_COLOR, 2)
-        x, y =ANCHOR_POINT
-        nx, ny = nose_point
-        w, h = 60, 35
-        multiple = 1
-        cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
-        cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
-
-        dir = direction(nose_point, ANCHOR_POINT, w, h)
-        cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
-        
-        drag = 25
-        if dir == 'right':
-            pag.moveRel(drag, 0)
-        elif dir == 'left':
-            pag.moveRel(-drag, 0)
-        elif dir == 'up':
-            if SCROLL_MODE:
-                pag.scroll(40)
-            else:
-                pag.moveRel(0, -drag)
-        elif dir == 'down':
-            if SCROLL_MODE:
-                pag.scroll(-40)
-            else:
-                pag.moveRel(0, drag)
-        # setAnchor(nx, ny)
-        print(ANCHOR_POINT ," ", nose_point)
-        
-    # for scrolling
-    if SCROLL_MODE:
-        cv2.putText(frame, 'SCROLLING', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
-        x, y =ANCHOR_POINT
-        nx, ny = nose_point
-        w, h = 60, 35
-        multiple = 1
-        cv2.rectangle(frame, (x - w, y - h), (x + w, y + h), GREEN_COLOR, 2)
-        cv2.line(frame, ANCHOR_POINT, nose_point, BLUE_COLOR, 2)
-
-        dir = direction(nose_point, ANCHOR_POINT, w, h)
-        cv2.putText(frame, dir.upper(), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, RED_COLOR, 2)
-        if dir == 'up':
-            pag.scroll(10)
-        elif dir == 'down':
-            pag.scroll(-10)
-
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-
-    # If the `Esc` key was pressed, break from the loop
-    if key == 27:
-        break
-
-
-cv2.destroyAllWindows()
-vid.release()
