eeec4e4f318017d8b719097cac8d375888928574
Dhruv Bansal <dhruvbansal13999@gmail.com>
2019-10-02 23:16:40 +0530

    Update gesture_mouse.py

--- gesture_mouse.py
+++ gesture_mouse.py
@@ -1,11 +1,12 @@
 from imutils import face_utils
-
+  
 from helper import *
 import numpy as np
 import pyautogui as pag
 import imutils
 import dlib
 import cv2    
+
 # Thresholds and consecutisve frame length for triggering the mouse action.
 
 MOUTH_AR_THRESH = 0.6
